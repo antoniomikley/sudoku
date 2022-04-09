fcd7150531b23578280492d0fa506f8ee9aa34d6
Antonio Mikley <antonio.mikley@gmail.com>
2022-04-09 16:56:47 +0200

    add first test

--- solver.py
+++ solver.py
@@ -1,2 +1,15 @@
 import pytest
 from sudoku import *
+
+def test_naked_single():
+    NS = Sudoku()
+    for n in range(1, 4):
+        NS.rows[0][n].assign_number(n)
+    for n in range(4, 7):
+        NS.columns[0][n].assign_number(n)
+    for n in range(7,9):
+        NS.regions[0][n].assign_number(n)
+    NS.solve_naked_single()
+    assert NS.squares[0].number == 9
+
+
