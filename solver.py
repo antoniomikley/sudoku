6bbdd95ad5675c909b3d341fbf8d3d889b88d7a8
Antonio Mikley <antonio.mikley@gmail.com>
2022-04-09 19:22:17 +0200

    add solving technique 'hidden single'

--- solver.py
+++ solver.py
@@ -6,3 +6,31 @@ def naked_single(sudoku_to_solve):
         if len(square.possible_numbers) == 1:
             square.assign_number(square.possible_numbers[0])
 
+def hidden_single(sudoku_to_solve):
+    grid = sudoku_to_solve
+    for i in range(0,9):
+        possible_num_in_col_row_reg = []
+        for square in grid.columns[i]:
+            if square.number == 0:
+                possible_num_in_col_row_reg.extend(square.possible_numbers)
+        for n in range(1, 10):
+            if possible_num_in_col_row_reg.count(n) == 1:
+                for square in grid.columns[i]:
+                    square.assign_number(n)
+        possible_num_in_col_row_reg = []
+        for square in grid.rows[i]:
+            if square.number == 0:
+                possible_num_in_col_row_reg.extend(square.possible_numbers)
+        for n in range(1, 10):
+            if possible_num_in_col_row_reg.count(n) == 1:
+                for square in grid.rows[i]:
+                    square.assign_number(n)
+        possible_num_in_col_row_reg = []
+        for square in grid.regions[i]:
+            if square.number == 0:
+                possible_num_in_col_row_reg.extend(square.possible_numbers)
+        for n in range(1, 10):
+            if possible_num_in_col_row_reg.count(n) == 1:
+                for square in grid.regions[i]:
+                    square.assign_number(n)
+
--- test_solver.py
+++ test_solver.py
@@ -2,6 +2,8 @@ import pytest
 from sudoku import *
 import solver
 
+# if there is only one possible number for a particular square,
+# then it is the right correct number for that square. 
 def test_naked_single():
     Ns = Sudoku()
     for n in range(1, 4):
@@ -12,5 +14,20 @@ def test_naked_single():
         Ns.regions[0][n].assign_number(n)
     solver.naked_single(Ns)
     assert Ns.squares[0].number == 9
+
+# if for a column, row or region, there is only one possibilty in which square
+# a particular number could be entered, then it is the right number for that square.
+# Test only covers the case, when the hidden single is deduced from the possible numbers
+# in a region, so cutting out the parts checking the rows and the columns wont affect the 
+# results of the test, but it also should not affect the functionality in a real world scenario.. hopefully..
+def test_hidden_single():
+    Ns = Sudoku()
+    Ns.rows[1][0].assign_number(1)
+    Ns.rows[1][2].assign_number(3)
+    Ns.rows[0][3].assign_number(2)
+    Ns.rows[2][8].assign_number(2)
+    solver.hidden_single(Ns)
+    assert Ns.rows[1][1].number == 2
+
 
 retcode = pytest.main(["-x", __file__])
